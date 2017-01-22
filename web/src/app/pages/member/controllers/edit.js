d723479c117c4a1ca1d1b7ca0f9b1c7114f04d80
Nilay Ölmez <nlyolmez@gmil.com>
2017-01-22 17:23:45 +0200

    added member pages views

--- web/src/app/pages/member/controllers/edit.js
+++ web/src/app/pages/member/controllers/edit.js
@@ -32,7 +32,16 @@
                     'Access-Control-Allow-Origin': '*'
                 },
                 data: {
-                    name: data.name
+                    first_name: member.first_name,
+                    last_name: member.last_name,
+                    mail: member.mail,
+                    phone_number: member.phone_number,
+                    gender: member.gender,
+                    address: member.address,
+                    city: member.city,
+                    state: member.state,
+                    country:member.country,
+                    zip_code: member.zip_code
                 }
             }).then(function successCallback(response) {
                 deferred.resolve(response);
